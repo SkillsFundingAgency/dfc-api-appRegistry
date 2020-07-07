c8f28f6eacbda1ac3a13c3c07c629a82418af213
Himanshu Trivedi <himanshu.trivedi@hotmail.com>
2020-07-07 13:26:57 +0100

    NCSD-4023-Initial Repo commit

---
+++ DFC.Api.AppRegistry.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DFC.Api.AppRegistry.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
