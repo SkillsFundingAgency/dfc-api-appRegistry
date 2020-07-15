65bb08a2e8a6b32b74c2bca6ecc6ce67f8e6cafb
Ian Crisp <icrisp@program-systems.co.uk>
2020-07-15 08:33:45 +0100

    Added PUT method

--- DFC.Api.AppRegistry/Functions/PutHttpTrigger.cs
+++ DFC.Api.AppRegistry/Functions/PutHttpTrigger.cs
@@ -15,37 +15,42 @@ using System.Threading.Tasks;
 
 namespace DFC.Api.AppRegistry.Functions
 {
-    public class PostHttpTrigger
+    public class PutHttpTrigger
     {
-        private readonly ILogger<PostHttpTrigger> logger;
+        private readonly ILogger<PutHttpTrigger> logger;
         private readonly IDocumentService<AppRegistrationModel> documentService;
 
-        public PostHttpTrigger(
-           ILogger<PostHttpTrigger> logger,
+        public PutHttpTrigger(
+           ILogger<PutHttpTrigger> logger,
            IDocumentService<AppRegistrationModel> documentService)
         {
             this.logger = logger;
             this.documentService = documentService;
         }
 
-        [FunctionName("Post")]
-        [Display(Name = "Post an app registration", Description = "Creates a new resource of type 'AppRegistry'.")]
-        [ProducesResponseType(typeof(AppRegistrationModel), (int)HttpStatusCode.Created)]
-        [Response(HttpStatusCode = (int)HttpStatusCode.Created, Description = "App Registration created", ShowSchema = true)]
-        [Response(HttpStatusCode = (int)HttpStatusCode.OK, Description = "App Registration updated", ShowSchema = false)]
+        [FunctionName("Put")]
+        [Display(Name = "Put an app registration", Description = "Updates a resource of type 'AppRegistry'.")]
+        [Response(HttpStatusCode = (int)HttpStatusCode.OK, Description = "App Registration updated", ShowSchema = true)]
+        [Response(HttpStatusCode = (int)HttpStatusCode.NoContent, Description = "Path does not exist", ShowSchema = false)]
         [Response(HttpStatusCode = (int)HttpStatusCode.Unauthorized, Description = "API key is unknown or invalid", ShowSchema = false)]
         [Response(HttpStatusCode = (int)HttpStatusCode.Forbidden, Description = "Insufficient access", ShowSchema = false)]
         [Response(HttpStatusCode = (int)HttpStatusCode.BadRequest, Description = "Request was malformed", ShowSchema = false)]
         [Response(HttpStatusCode = (int)HttpStatusCode.UnprocessableEntity, Description = "AppRegistry validation error(s)", ShowSchema = false)]
         [Response(HttpStatusCode = (int)HttpStatusCode.TooManyRequests, Description = "Too many requests being sent, by default the API supports 150 per minute.", ShowSchema = false)]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "appregistry/")] HttpRequest? request)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "appregistry/{path}")] HttpRequest? request, string path)
         {
-            logger.LogInformation("Validating Post AppRegistration for");
+            logger.LogInformation("Validating Put AppRegistration for");
 
             if (request == null)
             {
                 logger.LogWarning($"Missing request.Body");
+                return new BadRequestResult();
+            }
+
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                logger.LogWarning($"Missing value in request for '{nameof(path)}'");
                 return new BadRequestResult();
             }
 
@@ -71,32 +76,41 @@ namespace DFC.Api.AppRegistry.Functions
                 return new BadRequestResult();
             }
 
+            if (!string.Equals(path, body.Value.Path, StringComparison.OrdinalIgnoreCase))
+            {
+                logger.LogWarning($"Path parameter ({path}) does not match request value Path ({body.Value.Path})");
+                return new BadRequestResult();
+            }
+
+            logger.LogInformation($"Attempting to get app registration for: {path}");
+            var currentAppRegistration = await documentService.GetAsync(p => p.Path == path).ConfigureAwait(false);
+            if (currentAppRegistration == null)
+            {
+                logger.LogWarning($"No app registration exists for path: {path}");
+                return new NoContentResult();
+            }
+
             try
             {
-                logger.LogInformation($"Attempting to create app registration for: {body.Value.Path}");
+                logger.LogInformation($"Attempting to update app registration for: {body.Value.Path}");
 
-                body.Value.Regions?.ForEach(f => f.DateOfRegistration = DateTime.UtcNow);
-                body.Value.DateOfRegistration = DateTime.UtcNow;
+                body.Value.Regions?.ForEach(f => f.LastModifiedDate = DateTime.UtcNow);
+                body.Value.LastModifiedDate = DateTime.UtcNow;
 
                 var statusCode = await documentService.UpsertAsync(body.Value).ConfigureAwait(false);
 
-                switch (statusCode)
+                if (statusCode == HttpStatusCode.OK)
                 {
-                    case HttpStatusCode.Created:
-                        logger.LogInformation($"Created app registration with Post for: {body.Value.Path}: Status code {statusCode}");
-                        var appRegistrationModel = await documentService.GetAsync(p => p.Path == body.Value.Path).ConfigureAwait(false);
-                        return new CreatedResult(appRegistrationModel!.Path, appRegistrationModel);
-                    case HttpStatusCode.OK:
-                        logger.LogInformation($"Upserted app registration with Post for: {body.Value.Path}: Status code {statusCode}");
-                        return new OkResult();
+                    logger.LogInformation($"Upserted app registration with Put for: {body.Value.Path}: Status code {statusCode}");
+                    return new OkResult();
                 }
 
-                logger.LogError($"Error creating app registration with Post for: {body.Value.Path}: Status code {statusCode}");
+                logger.LogError($"Error updating app registration with Put for: {body.Value.Path}: Status code {statusCode}");
                 return new UnprocessableEntityResult();
             }
             catch (Exception ex)
             {
-                logger.LogError(ex, $"Error creating app registration with Post for: {body.Value.Path}");
+                logger.LogError(ex, $"Error updating app registration with Put for: {body.Value.Path}");
                 return new BadRequestResult();
             }
         }
