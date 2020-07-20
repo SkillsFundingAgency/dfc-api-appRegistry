c2d3c03091de588575e2184f01692f3e419ead28
Tom Tollafield <thomas.tollafield@gmail.com>
2020-07-20 11:49:59 +0100

    - Added tests

---
+++ DFC.Api.AppRegistry.UnitTests/ServicesTests/PagesWebhookServiceTests.cs
@@ -0,0 +1,86 @@
+﻿using Castle.Core.Logging;
+using DFC.Api.AppRegistry.Contracts;
+using DFC.Api.AppRegistry.Services;
+using FakeItEasy;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DFC.Api.AppRegistry.UnitTests.ServicesTests
+{
+    public class PagesWebhookServiceTests
+    {
+        private readonly ILogger<PagesWebhookService> logger;
+        private readonly IPagesDataLoadService pageDataLoadService;
+
+        public PagesWebhookServiceTests()
+        {
+            logger = A.Fake<ILogger<PagesWebhookService>>();
+            pageDataLoadService = A.Fake<IPagesDataLoadService>();
+        }
+
+        [Fact]
+        public async Task PagesWebhookServiceDeleteContentItemAsyncThrowsNotImplementedException()
+        {
+            // Arrange
+            var serviceToTest = new PagesWebhookService(pageDataLoadService, logger);
+
+            // Act
+            // Assert
+            await Assert.ThrowsAsync<NotImplementedException>(async () => await serviceToTest.DeleteContentItemAsync(Guid.NewGuid()).ConfigureAwait(false)).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task PagesWebhookServiceProcessContentItemAsyncThrowsNotImplementedException()
+        {
+            // Arrange
+            var serviceToTest = new PagesWebhookService(pageDataLoadService, logger);
+
+            // Act
+            // Assert
+            await Assert.ThrowsAsync<NotImplementedException>(async () => await serviceToTest.ProcessContentItemAsync(new Uri("http://somewhere.com"), Guid.NewGuid()).ConfigureAwait(false)).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task PagesWebhookServiceProcessMessageAsyncThrowsNotImplementedException()
+        {
+            // Arrange
+            var serviceToTest = new PagesWebhookService(pageDataLoadService, logger);
+
+            // Act
+            // Assert
+            await Assert.ThrowsAsync<NotImplementedException>(async () => await serviceToTest.ProcessMessageAsync(Compui.Subscriptions.Pkg.Data.Enums.WebhookCacheOperation.CreateOrUpdate, Guid.NewGuid(), Guid.NewGuid(), new Uri("http://somewhere.com")).ConfigureAwait(false)).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task PagesWebhookServiceDeleteContentAsyncReturnsOkStatus()
+        {
+            // Arrange
+            var serviceToTest = new PagesWebhookService(pageDataLoadService, logger);
+            A.CallTo(() => pageDataLoadService.RemoveAsync(A<Guid>.Ignored)).Returns(System.Net.HttpStatusCode.OK);
+
+            // Act
+            var result = await serviceToTest.DeleteContentAsync(Guid.NewGuid()).ConfigureAwait(false);
+
+            // Assert
+            Assert.Equal(System.Net.HttpStatusCode.OK, result);
+            A.CallTo(() => pageDataLoadService.RemoveAsync(A<Guid>.Ignored)).MustHaveHappenedOnceExactly();
+        }
+
+        [Fact]
+        public async Task PagesWebhookServiceProcessContentAsyncReturnsOkStatus()
+        {
+            // Arrange
+            var serviceToTest = new PagesWebhookService(pageDataLoadService, logger);
+            A.CallTo(() => pageDataLoadService.CreateOrUpdateAsync(A<Guid>.Ignored)).Returns(System.Net.HttpStatusCode.OK);
+
+            // Act
+            var result = await serviceToTest.ProcessContentAsync(new Uri("http://somewhere.com"), Guid.NewGuid()).ConfigureAwait(false);
+
+            // Assert
+            Assert.Equal(System.Net.HttpStatusCode.OK, result);
+            A.CallTo(() => pageDataLoadService.CreateOrUpdateAsync(A<Guid>.Ignored)).MustHaveHappenedOnceExactly();
+        }
+    }
+}
--- DFC.Api.AppRegistry/Services/PagesWebhookService.cs
+++ DFC.Api.AppRegistry/Services/PagesWebhookService.cs
@@ -1,6 +1,7 @@
 ﻿using DFC.Api.AppRegistry.Contracts;
 using DFC.Compui.Subscriptions.Pkg.Data.Contracts;
 using DFC.Compui.Subscriptions.Pkg.Data.Enums;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Net;
 using System.Threading.Tasks;
@@ -10,14 +11,18 @@ namespace DFC.Api.AppRegistry.Services
     public class PagesWebhookService : IWebhooksService
     {
         private readonly IPagesDataLoadService pagesDataLoadService;
+        private readonly ILogger<PagesWebhookService> logger;
 
-        public PagesWebhookService(IPagesDataLoadService pagesDataLoadService)
+        public PagesWebhookService(IPagesDataLoadService pagesDataLoadService, ILogger<PagesWebhookService> logger)
         {
             this.pagesDataLoadService = pagesDataLoadService;
+            this.logger = logger;
         }
 
         public async Task<HttpStatusCode> DeleteContentAsync(Guid contentId)
         {
+            logger.LogInformation($"{nameof(DeleteContentAsync)} called in {nameof(PagesWebhookService)}");
+
             var result = await pagesDataLoadService.RemoveAsync(contentId).ConfigureAwait(false);
             return result;
         }
@@ -27,9 +32,12 @@ namespace DFC.Api.AppRegistry.Services
             throw new NotImplementedException();
         }
 
-        public Task<HttpStatusCode> ProcessContentAsync(Uri url, Guid contentId)
+        public async Task<HttpStatusCode> ProcessContentAsync(Uri url, Guid contentId)
         {
-            throw new NotImplementedException();
+            logger.LogInformation($"{nameof(ProcessContentAsync)} called in {nameof(PagesWebhookService)}  with Content Id {contentId}");
+
+            var result = await pagesDataLoadService.CreateOrUpdateAsync(contentId).ConfigureAwait(false);
+            return result;
         }
 
         public Task<HttpStatusCode> ProcessContentItemAsync(Uri url, Guid contentItemId)
@@ -37,16 +45,9 @@ namespace DFC.Api.AppRegistry.Services
             throw new NotImplementedException();
         }
 
-        public async Task<HttpStatusCode> ProcessMessageAsync(WebhookCacheOperation webhookCacheOperation, Guid eventId, Guid contentId, Uri url)
+        public Task<HttpStatusCode> ProcessMessageAsync(WebhookCacheOperation webhookCacheOperation, Guid eventId, Guid contentId, Uri url)
         {
-            //Process the message here
-            if (webhookCacheOperation == WebhookCacheOperation.CreateOrUpdate)
-            {
-                var result = await pagesDataLoadService.CreateOrUpdateAsync(contentId).ConfigureAwait(false);
-                return result;
-            }
-
-            return HttpStatusCode.OK;
+            throw new NotImplementedException();
         }
     }
 }
