177e6b993399282918fe1c28a83dbbb5667f02e2
Ian Crisp <49161034+IanCrisp@users.noreply.github.com>
2020-07-22 14:38:48 +0100

    Story dfcc 667 update page locations (#35)
    * updated page location handling
    
    * updated nugets

--- DFC.Api.AppRegistry/Models/Pages/PageModel.cs
+++ DFC.Api.AppRegistry/Models/Pages/PageModel.cs
@@ -1,14 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DFC.Api.AppRegistry.Models.Pages
 {
     public class PageModel
     {
-        public string? CanonicalName { get; set; }
+        public Guid? Id { get; set; }
+
+        public string? Location { get; set; }
 
         public Uri? Url { get; set; }
 
         public IList<string>? RedirectLocations { get; set; }
+
+        public List<string>? AllLocations
+        {
+            get
+            {
+                var result = new List<string>();
+
+                if (!string.IsNullOrWhiteSpace(Location))
+                {
+                    result.Add(Location);
+                }
+
+                if (RedirectLocations != null && RedirectLocations.Any())
+                {
+                    result.AddRange(RedirectLocations);
+                }
+
+                return result;
+            }
+        }
     }
 }
